daf1bacbc7d2c44b48984c2b1e92a28b0a585be7
Ivan <ivanjosevera28@gmail.com>
2023-12-01 14:35:52 -0500

    corrected an issue with the URI

--- app/(models)/Ticket.js
+++ app/(models)/Ticket.js
@@ -4,7 +4,7 @@ process.on("uncaughtException", function (err) {
   console.log(err);
 });
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 
 const ticketSchema = new Schema(
